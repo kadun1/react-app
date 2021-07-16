34fd60040d873efdb647ced213a4a39f74c06d91
kadun1 <kadunjj@gmail.com>
2021-07-17 01:02:51 +0900

    react 3day

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 import {Component} from "react";
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.max_content_id = 3;
         this.state = {
-            mode:'read',
+            mode:'create',
             selected_content_id:2,
             subject:{title:'WEB', sub:'World Wide Web!'},
-            welcome:{title:'Welcome', desc:'Hello, Recat!!'},
+            welcome:{title:'Welcome', desc:'Hello, React!!'},
             contents:[
                 {id:1, title:'HTML', desc:'HTML is HyperText ...' },
                 {id:2, title:'CSS', desc:'CSS is for design' },
@@ -21,10 +24,11 @@ class App extends Component {
     }
     render() {
         console.log('App render');
-        var _title, _desc = null;
+        var _title, _desc, _article = null;
         if(this.state.mode === 'welcome'){
             _title = this.state.welcome.title;
             _desc = this.state.welcome.title;
+            _article = <ReadContent title={_title} desc={_desc}></ReadContent>
         } else if(this.state.mode === 'read'){
             var i = 0;
             while (i < this.state.contents.length){
@@ -36,6 +40,23 @@ class App extends Component {
                 }
                 i = i + 1;
             }
+            _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+        } else if(this.state.mode === 'create'){
+            _article = <CreateContent onSubmit={function (_title, _desc){
+                this.max_content_id += 1;
+                // this.state.contents.push(
+                //     {id:this.max_content_id, title:_title, desc:_desc}
+                // );
+                var _contents = this.state.contents.concat(
+                    {id:this.max_content_id, title:_title, desc:_desc}
+                )
+                this.setState({
+                    contents: _contents
+                })
+
+                console.log(_title, _desc);
+            }.bind(this)
+            }></CreateContent>
         }
         console.log('render',this);
         return (
@@ -55,7 +76,13 @@ class App extends Component {
                 })
             }.bind(this)}
                  data={this.state.contents}></TOC>
-            <Content title={_title} desc={_desc}></Content>
+            <Control onChangeMode={function (_mode){
+                this.setState({
+                    mode:_mode
+                })
+            }.bind(this)
+            }></Control>
+            {_article}
         </div>
         );
     }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,8 +1,17 @@
 import {Component} from "react";
 
 class TOC extends Component{
+    shouldComponentUpdate(newProps, newState) {
+        console.log('=======>TOC render shouldComponent');
+        if(newProps.data === this.props.data){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
     render() {
-        console.log('TOC render');
+        console.log('=======>TOC render');
         var lists = [];
         var data = this.props.data;
         var i =0;
